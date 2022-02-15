696ff43838c26e270ead958f226ce926b050bf4c
alvaroascenco <alvaroascenco16@outlook.com>
2022-02-14 22:49:17 -0300

    passed the test

--- src/ProductStructure.php
+++ src/ProductStructure.php
@@ -20,7 +20,40 @@ class ProductStructure
 
     public function make(): array
     {
-       //todo your code.
-        return [];
+        $colorSizeArrayUnformatted = self::products;
+        $colorSizeArrayFormatted = [];
+
+        //declaring array length as variable so PHP doesnt evaluate it over again everytime it goes through the loop
+        $colorSAULength = count($colorSizeArrayUnformatted);
+        $index = 0;
+
+        while($index < $colorSAULength){
+            $entryExploded = explode('-', $colorSizeArrayUnformatted[$index]);
+
+            $colorName = $entryExploded[0];
+
+            if(isset($colorSizeArrayFormatted[$colorName])){
+                $index++;
+                continue;
+            }
+            $colorSizeArrayFormatted[$colorName] = [];
+
+            while($index < $colorSAULength){
+
+                $entryColorExploded = explode('-', $colorSizeArrayUnformatted[$index]);
+                $entryColorName = $entryColorExploded[0];
+                
+                if($entryColorName != $colorName) break;
+                $entryColorSize = $entryColorExploded[1];
+               
+                if(!isset($colorSizeArrayFormatted[$colorName][$entryColorSize])) 
+                    $colorSizeArrayFormatted[$colorName][$entryColorSize] = 1;
+                else
+                    $colorSizeArrayFormatted[$colorName][$entryColorSize]++;
+                
+                $index++;
+            }
+        }
+        return $colorSizeArrayFormatted;
     }
 }
